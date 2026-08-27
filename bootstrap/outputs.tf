c26f43b3b5ead93140c4db7aaf3b48a81d271aa9
cisse17 <119404406+cisse17@users.noreply.github.com>
2026-08-27 18:22:58 +0200

    feat: OIDC (OpenID connect)

--- bootstrap/outputs.tf
+++ bootstrap/outputs.tf
@@ -1,3 +1,8 @@
 output "s3_bucket_name" {
     value = aws_s3_bucket.terraform_state.bucket
 }
+
+output "github_actions_role_arn" {
+  value = aws_iam_role.github_actions.arn
+  description = "ARN du rôle à mettre dans le pipeline"
+}
